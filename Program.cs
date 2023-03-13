6d9eaf1abcdd471300db8b9e2e092f65dcf44c0c
PeemmegaTH <theeridth.yodk@kmutt.ac.th>
2023-03-13 15:18:54 +0700

    Final P2

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Project {
+    class First {
+        static void Main(string[] args){
+            Console.Write("Input Day of Week: ");
+            int d = int.Parse(Console.ReadLine()); // X Position
+            Console.Write("Input Time of Day: "); 
+            int t = int.Parse(Console.ReadLine()); // Y Position
+
+            int b = 5;
+            int w = 2;
+            int c = 3;
+
+            bool end = false;
+            while (!end) {
+                Console.Write("Input Menu: "); 
+
+                switch(Console.ReadLine()) {
+                
+                    case "Breakfast Set":
+                        if (b > 0) {
+                           if (t > 11) {
+                            Console.WriteLine(" Sorry your order is not available");
+                           } else {
+                             Console.WriteLine("Order Surcess");
+                             --b;
+                           }
+
+                        } else {
+                            Console.WriteLine("Sorry your order is out of stock");
+                        }
+                        break;
+                    case "Weekend Set":
+                        if (w > 0) {
+                           if (d < 6) {
+                            Console.WriteLine("Sorry your order is not available");
+                        
+                           } else {
+                             Console.WriteLine("Order Surcess");
+                             --w;
+                           }
+
+                        } else {
+                            Console.WriteLine("Sorry your order is out of stock");
+                        }
+                        break;
+                    case "Coffee":
+                        if (c > 0) {
+                           Console.WriteLine("Order Surcess");
+                             --c;
+                        } else {
+                            Console.WriteLine("Sorry your order is out of stock");
+                        }
+                        break;          
+                    case "End":
+                        end = true;
+                        break;
+                    default:
+                        Console.WriteLine("Please enter a valid menu");
+                        break;    
+                }
+            }
+        }
+
+    }
+}
